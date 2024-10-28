d361f282b2ba92e035c1848f2747311f2a18c52e
yoelsirait <yoelsirait05@gmail.com>
2024-10-28 16:41:45 +0700

    add file style css

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Customer Sales Order Application</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Customer Sales Order</h1>
+        <p>Welcome to the Customer Sales Order application. This application helps manage and process customer orders efficiently.</p>
+        <p>Choose one of the following options:</p>
+        
+        <div class="buttons">
+            <a href="addProduct.php" class="button">Add Product</a>
+            <a href="salesOrder.php" class="button">Add Order</a>
+        </div>
+    </div>
+</body>
+</html>
